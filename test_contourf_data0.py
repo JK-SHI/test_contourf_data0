d5820d9409d9f1015c1f5235cb23ba88dd4628c5
JK-SHI <92288324+JK-SHI@users.noreply.github.com>
2021-10-20 00:15:50 +0900

    Update test_contourf_data0.py
    Provide a new sample program to confirm the problem with data 0.

--- test_contourf_data0.py
+++ test_contourf_data0.py
@@ -3,21 +3,36 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    z0 = np.array([[0., 0., 2.], [0., 0., 1.], [2., 1., 0.]])
-    z1 = np.array([[0., 0., 2.], [0., 0., 1.], [2., 1., -1.7E-13]])
+    x = np.linspace(-1, 1, 21)
+    y = np.linspace(-1, 1, 21)
+    x, y = np.meshgrid(x, y)
+    z0 = x * x + y * y
 
-    fig = plt.figure(figsize=(2, 4))
+    for i in range(1, 5):
+        for j in range(1, 5):
+            z0[i, j] = 0.0
+
+    for i in range(16, 19):
+        for j in range(2, 5):
+            z0[i, j] = np.nan
+
+    z1 = z0.copy()
+    for i in range(2, 5):
+        for j in range(16, 19):
+            z1[i, j] = -1.7E-13
+
+    fig = plt.figure(figsize=(4, 8))
 
     ax0 = fig.add_subplot(121)
-    ax0.contourf(z0, cmap='jet')
+    ax0.contourf(x, y, z0, cmap='jet')
     ax0.set_aspect('equal')
 
     ax1 = fig.add_subplot(122)
-    ax1.contourf(z1, cmap='jet')
+    ax1.contourf(x, y, z1, cmap='jet')
     ax1.set_aspect('equal')
 
     plt.show()
-
+    
 
 if __name__ == "__main__":
     main()
